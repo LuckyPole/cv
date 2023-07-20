177df081e5d51cc8b149c685fc334046a86382f1
munga06 <munga21407@gmail.com>
2023-07-20 16:37:43 +0300

    my latest

--- interactive.js
+++ interactive.js
@@ -1,5 +1,4 @@
 const dropdownItems = document.querySelectorAll('.dropdown');
-// Add event listeners to each dropdown parent item
 dropdownItems.forEach((item) => {
   item.addEventListener('mouseenter', () => {
     item.querySelector('.dropdown-menu').style.display = 'block';
