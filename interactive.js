caf08a96fe2160bc15994a597130c6f15b3a4764
munga06 <munga21407@gmail.com>
2023-07-20 18:52:38 +0300

    sawa

--- interactive.js
+++ interactive.js
@@ -1,10 +1,25 @@
-const dropdownItems = document.querySelectorAll('.dropdown');
-dropdownItems.forEach((item) => {
-  item.addEventListener('mouseenter', () => {
-    item.querySelector('.dropdown-menu').style.display = 'block';
-  });
+// Get the dropdown parent element
+const dropdown = document.querySelector('.dropdown');
 
-  item.addEventListener('mouseleave', () => {
-    item.querySelector('.dropdown-menu').style.display = 'none';
+// Get the menu icon element
+const menuIcon = document.querySelector('.menu-icon');
+
+// Add event listeners to show/hide the menu icon
+dropdown.addEventListener('mouseenter', () => {
+  menuIcon.style.display = 'block';
+});
+
+dropdown.addEventListener('mouseleave', () => {
+  menuIcon.style.display = 'none';
+});
+
+// Get all the menu icon links
+const menuIconLinks = document.querySelectorAll('.menu-icon a');
+
+// Add event listeners to open links in a new tab/window
+menuIconLinks.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.stopPropagation();
   });
 });
+
