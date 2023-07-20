cb3a81ab76c3ce6e2de1458b2c6fbc9f6529f090
munga06 <munga21407@gmail.com>
2023-07-20 16:03:56 +0300

    some changes

--- interactive.js
+++ interactive.js
@@ -1,8 +1,4 @@
-// Add this script at the end of the HTML body or in a separate JavaScript file
-
-// Get all the dropdown parent items
 const dropdownItems = document.querySelectorAll('.dropdown');
-
 // Add event listeners to each dropdown parent item
 dropdownItems.forEach((item) => {
   item.addEventListener('mouseenter', () => {
